b9da78adb97b7b2cb0654a0d823d6b37dc35db8d
kingchikason <popechikason@gmail.com>
2021-01-14 09:30:49 +0100

    first commit of the project

---
+++ app/src/main/java/com/chika/decagontest/activities/Cars.java
@@ -0,0 +1,17 @@
+package com.chika.decagontest.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chika.decagontest.R;
+
+public class Cars extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_display);
+    }
+
+}
---
+++ app/src/main/java/com/chika/decagontest/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.chika.decagontest.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chika.decagontest.R;
+
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
